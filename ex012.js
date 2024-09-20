e885f0465861cc5067f6941eb4c6742a132c5a3c
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-09-20 20:36:18 -0300

    commit

--- ex012.js
+++ ex012.js
@@ -6,10 +6,4 @@ if (hora <= 12) {
 }
 else if (hora >= 12) {
     console.log('Boa tarde!')
-    if (hora >= 18) {
-        console.log('Boa noite!')
-    }
-    else {
-        console.log('')
-    }
 }
