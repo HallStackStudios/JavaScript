0044e074cf9fa9a7344540e1803d1a40ab92ed13
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-09-20 12:18:25 -0300

    commit

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel} kilometros por hora`)
+if (vel >= 120) {
+    console.log('Você ultrapassou a velocidade permitida!, MULTADO!')
+}
+console.log('Diriga sempre usando cinto de segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = "Brasil"
+console.log(`Vivendo em ${pais}`)
+if (pais == "Brasil") {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Estrangeiro!!!')
+}
