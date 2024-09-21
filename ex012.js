da4c1fd7bdf3dc3c1fc1523777bea4a4c45ee8f5
asafegithubs <g.a.oliveirastudios@gmail.com>
2024-09-21 10:23:07 -0300

    commit

--- ex012.js
+++ ex012.js
@@ -1,9 +1,12 @@
-//codigo ainda sendo feito 
-var hora = 19
+var agora = new Date()
+var hora = agora.getHours()
 console.log(`Agora são exatamente ${hora} horas`)
-if (hora <= 12) {
+if (hora < 12 ) {
     console.log('Bom Dia!')
 }
-else if (hora >= 12) {
+else if (hora <= 18) {
     console.log('Boa tarde!')
+}
+else {
+    console.log('Boa noite!')
 }
