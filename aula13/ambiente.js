a10ce2c505d449dad822f7452555cf9a773fa0e0
asafegithubs <g.a.oliveirastudios@gmail.com>
2025-01-30 10:17:53 -0300

    commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
+
+
+
+
+
+/* ESTE É O CODIGO SEM USAR O WHILE
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+*/
---
+++ aula13/node_ab1e79420137d.tmp.js
@@ -0,0 +1,20 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 9999999999) {
+    
+}
+
+
+
+
+
+/* ESTE É O CODIGO SEM USAR O WHILE
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+console.log("teste de aula de repetição while!")
+*/
