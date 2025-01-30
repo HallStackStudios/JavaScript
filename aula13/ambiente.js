0093189e5a2b8a9cf4aa39747ced7ea8f6d49f61
asafegithubs <g.a.oliveirastudios@gmail.com>
2025-01-30 10:54:31 -0300

    commit

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,13 +1,10 @@
-var c = 1
-do {
-    console.log(`Passo ${c}`)
-    c++
-} while (c <= 10)
-
-
-
-
-
+var static = 16.24;
+var money = 2.0;
+while (static <= 364.74) {
+    console.log(`Você acabou de ganhar mais $${money.toFixed(2)}, agora você possui $${(static + money).toFixed(2)}`);
+    static += money; // Atualiza o saldo
+    money += 0.50; // Aumenta o valor ganho a cada loop
+}
 /* ESTE É O CODIGO SEM USAR O WHILE
 console.log("teste de aula de repetição while!")
 console.log("teste de aula de repetição while!")
